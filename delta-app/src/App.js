6649232d7505e6a390430866f9f77d48c9466449
cjhet02 <cjhet02@gmail.com>
2024-02-27 22:40:39 -0800

    feat: basic frontend with button that calls champDelta

--- delta-app/src/App.js
+++ delta-app/src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import * as patchUtil from './patchUtil.js';
+
+import { useState } from "react";
 
 function App() {
+  const [delta, setDelta] = useState("");
+
+  const handleDelta = async () => {
+    const res = await patchUtil.champDelta(13, 20, 14, 4, 'Illaoi');
+    setDelta(JSON.stringify(res, null, 2));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>fuck it we ball</h1>
+      <p>{ delta }</p>
+      <button onClick={handleDelta}>Get Patch</button>
     </div>
   );
 }
