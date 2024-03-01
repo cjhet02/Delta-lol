7502522f7d11a52afb1274556cd12c7212f9ae42
Jhet Cabigas <cjhet02@gmail.com>
2024-03-01 11:19:26 -0800

    Rough frontend patch display

--- delta-app/src/App.js
+++ delta-app/src/App.js
@@ -5,18 +5,43 @@ import * as patchUtil from './patchUtil.js';
 import { useState } from "react";
 
 function App() {
-  const [delta, setDelta] = useState("");
+  const [delta, setDelta] = useState({champ: "", changeList: []});
   const [champ, setChamp] = useState("");
   const [start, setStart] = useState("");
   const [end, setEnd] = useState("");
+
+  const ChangeItem = ({ change, values }) => (
+    <div>
+      <h3>{change}</h3>
+      <ul>
+        {values.map((value, index) => (
+          <li key={index}>
+            <h4>{value.feature}</h4>
+            <div>
+              {value.before.join(', ')} ⇒ {value.after.join(', ')}<br />
+              <strong>Delta:</strong> {value.delta.join(', ')}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+  
+  const ChampionChanges = ({ champ, changeList }) => (
+    <div>
+      <h2>{champ} Changes</h2>
+      <strong>Before {start} ⇒ After {end}</strong> <br />
+      {changeList.map((changeItem, index) => (
+        <ChangeItem key={index} {...changeItem} />
+      ))}
+    </div>
+  );
   
   const handleDelta = async () => {
-    console.log('handling');
     const sSplit = start.split('.');
     const eSplit = end.split('.');
-    console.log(sSplit, eSplit);
     const res = await patchUtil.champDelta(parseInt(sSplit[0]), parseInt(sSplit[1]), parseInt(eSplit[0]), parseInt(eSplit[1]), champ);
-    setDelta(JSON.stringify(res, null, 2));
+    setDelta(res);
   };
 
   return (
@@ -26,9 +51,11 @@ function App() {
         <input value={start} onChange={e => setStart(e.target.value)}/>
         <input value={end} onChange={e => setEnd(e.target.value)}/>
       </div>
-      <input value={champ} onChange={e => setChamp(e.target.value)}/>
-      <p>{ delta }</p>
+      <input value={champ} onChange={e => setChamp(e.target.value)} />
       <button onClick={handleDelta}>Get Patch</button>
+      <div>
+        <ChampionChanges { ...delta }/>
+      </div>
     </div>
   );
 }
